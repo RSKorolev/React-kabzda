5f256eb38f6c810e990e2a5d075de7f144c34649
Ruslan Korolev <RSKorolev@bk.ru>
2024-03-30 14:17:30 +0700

    callbacks  UncontrolleRating

--- src/components/UncontrolleRating/UncontrolleRating.tsx
+++ src/components/UncontrolleRating/UncontrolleRating.tsx
@@ -6,31 +6,25 @@ function UncontrolleRating(props: RatingPropsType) {
     let [value, setValue] = useState(0);
     return (
         <div>
-            <Star selected={value > 0} />
-            <button onClick={() => setValue(1)}>1</button>
-            <Star selected={value > 1} />
-            <button onClick={() => setValue(2)}>2</button>
-            <Star selected={value > 2} />
-            <button onClick={() => setValue(3)}>3</button>
-            <Star selected={value > 3} />
-            <button onClick={() => setValue(4)}>4</button>
-            <Star selected={value > 4} />
-            <button onClick={() => setValue(5)}>5</button>
+            <Star selected={value > 0} setValue={setValue} value={1} />
+            <Star selected={value > 1} setValue={setValue} value={2} />
+            <Star selected={value > 2} setValue={setValue} value={3} />
+            <Star selected={value > 3} setValue={setValue} value={4} />
+            <Star selected={value > 4} setValue={setValue} value={5} />
         </div>
     );
 }
 
 type StarPropsType = {
     selected: true | false;
+    value: 0 | 1 | 2 | 3 | 4 | 5;
+    setValue: (value: 0 | 1 | 2 | 3 | 4 | 5) => void;
 };
-function Star({ selected }: StarPropsType) {
-    return selected ? (
-        <span>
-            <b>Star</b>
+function Star({ selected, setValue, value }: StarPropsType) {
+    return (
+        <span onClick={() => setValue(value)}>
+            {selected ? <b> Star </b> : 'Star'}
         </span>
-    ) : (
-        <span>Star</span>
     );
 }
-
 export default UncontrolleRating;
